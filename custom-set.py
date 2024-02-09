94a9ea9cd123f4ee1505b874f8ed41ab107a1d6f
sampsn <g@samp.sn>
2024-02-09 10:04:33 -0700

    Add custom-set.py, which is the completed assignment.

---
+++ custom-set.py
@@ -0,0 +1,40 @@
+class CustomSet():
+    list = []
+    def __init__(self):
+        pass
+
+    def add(self, item):
+        if item not in self.list:
+            self.list.append(item)
+
+    def remove(self, item):
+        if item in self.list:
+            self.list.remove(item)
+
+    def as_list(self):
+        return self.list
+
+    def clear(self):
+        self.list = []
+
+def main():
+    my_set = CustomSet()
+    my_set.add("item 1")
+    my_set.add("item 2")
+    my_set.add("item 1")
+
+    print(my_set.as_list())
+
+    my_set.remove("item 2")
+    print(my_set.as_list())
+
+    try:
+        my_set.remove("item 2")
+    except KeyError:
+        print("Item not removed, moving forward")
+
+    my_set.clear()
+    print(my_set.as_list())
+
+if __name__ == "__main__":
+    main()
